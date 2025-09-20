19bc037edeb935d5b958ca2cbce07a3db266b034
Evelez23 <elmer_velez@hotmail.com>
2025-09-20 12:42:58 -0600

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,37 @@
+// src/main.js
+import BootScene from './scenes/BootScene.js';
+import PreloadScene from './scenes/PreloadScene.js';
+import MenuScene from './scenes/MenuScene.js';
+import GameScene from './scenes/GameScene.js';
+import BossScene from './scenes/BossScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    backgroundColor: '#87CEEB',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 800 },
+            debug: false
+        }
+    },
+    scene: [BootScene, PreloadScene, MenuScene, GameScene, BossScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+};
+
+const game = new Phaser.Game(config);
+
+// Variables globales
+window.gameState = {
+    currentLevel: '1-1',
+    score: 0,
+    lives: 3,
+    heartsCollected: 0,
+    soundEnabled: true
+};
