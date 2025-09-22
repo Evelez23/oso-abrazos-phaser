2c05b6382e4f12ed19978d75f87182621c84f646
Evelez23 <elmer_velez@hotmail.com>
2025-09-22 07:53:57 -0600

    Update BootScene.js

--- src/ scenes/BootScene.js
+++ src/ scenes/BootScene.js
@@ -1,11 +1,9 @@
-// src/scenes/BootScene.js
-export default class BootScene extends Phaser.Scene {
+class BootScene extends Phaser.Scene {
     constructor() {
         super({ key: 'BootScene' });
     }
 
     preload() {
-        // Cargar assets mínimos para la pantalla de carga
         this.load.image('loading-bg', 'assets/images/loading-bg.png');
         this.load.image('logo', 'assets/images/logo.png');
     }
