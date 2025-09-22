35da2ebbaa151bd01de7cc07be31404ab50e9466
Evelez23 <elmer_velez@hotmail.com>
2025-09-22 07:54:45 -0600

    Update MenuScene.js

--- src/ scenes/MenuScene.js
+++ src/ scenes/MenuScene.js
@@ -1,14 +1,11 @@
-// src/scenes/MenuScene.js
-export default class MenuScene extends Phaser.Scene {
+class MenuScene extends Phaser.Scene {
     constructor() {
         super({ key: 'MenuScene' });
     }
 
     create() {
-        // Fondo del menú
         this.add.image(400, 300, 'forest1').setDisplaySize(800, 600);
         
-        // Título
         this.add.text(400, 150, 'OSO ABRAZOS', {
             fontSize: '48px',
             fill: '#fff',
@@ -17,7 +14,6 @@ export default class MenuScene extends Phaser.Scene {
             fontStyle: 'bold'
         }).setOrigin(0.5);
         
-        // Botón de jugar
         const playButton = this.add.text(400, 300, 'JUGAR', {
             fontSize: '32px',
             fill: '#fff',
@@ -37,18 +33,6 @@ export default class MenuScene extends Phaser.Scene {
         
         playButton.on('pointerdown', () => {
             this.scene.start('GameScene', { level: '1-1' });
-        });
-        
-        // Botón de sonido
-        const soundText = window.gameState.soundEnabled ? '🔊 SONIDO' : '🔇 SILENCIO';
-        const soundButton = this.add.text(400, 370, soundText, {
-            fontSize: '24px',
-            fill: '#fff'
-        }).setOrigin(0.5).setInteractive();
-        
-        soundButton.on('pointerdown', () => {
-            window.gameState.soundEnabled = !window.gameState.soundEnabled;
-            soundButton.setText(window.gameState.soundEnabled ? '🔊 SONIDO' : '🔇 SILENCIO');
         });
     }
 }
