71e1fda280095c3c73f9cadb48d8ca4eedf9c5e9
Evelez23 <elmer_velez@hotmail.com>
2025-09-22 07:57:58 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-// src/main.js
-import BootScene from './scenes/BootScene.js';
-import PreloadScene from './scenes/PreloadScene.js';
-import MenuScene from './scenes/MenuScene.js';
-import GameScene from './scenes/GameScene.js';
-import BossScene from './scenes/BossScene.js';
-
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -18,7 +11,7 @@ const config = {
             debug: false
         }
     },
-    scene: [BootScene, PreloadScene, MenuScene, GameScene, BossScene],
+    scene: [BootScene, PreloadScene, MenuScene, GameScene, BossScene, VictoryScene],
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
@@ -27,7 +20,6 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-// Variables globales
 window.gameState = {
     currentLevel: '1-1',
     score: 0,
