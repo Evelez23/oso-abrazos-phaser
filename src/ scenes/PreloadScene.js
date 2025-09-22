cdf3769e31b701bea3c9845eb9f72814c94445cd
Evelez23 <elmer_velez@hotmail.com>
2025-09-22 07:31:53 -0600

    Update PreloadScene.js

--- src/ scenes/PreloadScene.js
+++ src/ scenes/PreloadScene.js
@@ -5,74 +5,63 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        // Mostrar barra de progreso
         this.createProgressBar();
-
-        // Cargar todos los assets
         this.loadAssets();
     }
 
     createProgressBar() {
-        const width = 400;
-        const height = 20;
-        const x = (this.sys.game.config.width - width) / 2;
-        const y = (this.sys.game.config.height - height) / 2 + 100;
-
-        // Fondo de la barra
-        this.add.rectangle(x, y, width, height, 0x666666)
-            .setOrigin(0, 0.5);
-
-        // Barra de progreso
-        const progressBar = this.add.rectangle(x, y, 0, height, 0x00FF00)
-            .setOrigin(0, 0.5);
-
-        // Texto de progreso
-        const progressText = this.add.text(
-            this.sys.game.config.width / 2,
-            y + 30,
-            '0%',
-            { fontSize: '18px', fill: '#fff' }
-        ).setOrigin(0.5);
-
-        // Actualizar progreso
-        this.load.on('progress', (value) => {
-            progressBar.width = width * value;
-            progressText.setText(`${Math.round(value * 100)}%`);
-        });
+        // ... (código igual que antes)
     }
 
     loadAssets() {
-        // Cargar sprites del oso
-        this.load.image('oso_idle', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/oso/oso_idle.svg');
-        this.load.image('oso_walk', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/oso/oso_walk.svg');
-        this.load.image('oso_jump', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/oso/oso_jump.svg');
-        this.load.image('oso_hug', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/oso/oso_hugging.svg');
-        this.load.image('osoyuni', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/Amigos/osoyuni.png');
+        // SPRITES DEL OSO - Rutas locales
+        this.load.image('oso_idle', 'assets/images/oso/oso_idle.svg');
+        this.load.image('oso_walk', 'assets/images/oso/oso_walk.svg');
+        this.load.image('oso_jump', 'assets/images/oso/oso_jump.svg');
+        this.load.image('oso_hug', 'assets/images/oso/oso_hugging.svg');
+        this.load.image('oso_sit', 'assets/images/oso/oso_sit.svg');
+        this.load.image('oso_run', 'assets/images/oso/oso_run.svg');
+        this.load.image('osoyuni', 'assets/images/oso/osoyuni.png');
 
-        // Cargar amigos
-        this.load.image('squirrel', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/Amigos/ardilla.png');
-        this.load.image('rabbit', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/Amigos/conejo.png');
-        this.load.image('bird', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/Amigos/pajarito.png');
-        this.load.image('heart', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/Amigos/lovepower.png');
+        // AMIGOS - Rutas locales
+        this.load.image('squirrel', 'assets/images/amigos/ardilla.png');
+        this.load.image('rabbit', 'assets/images/amigos/conejo.png');
+        this.load.image('bird', 'assets/images/amigos/pajarito.png');
+        this.load.image('heart', 'assets/images/amigos/lovepower.png');
+        this.load.image('unicorn', 'assets/images/amigos/unirshup.png');
+        this.load.image('honey', 'assets/images/amigos/miel.png');
 
-        // Cargar enemigos
-        this.load.image('enemy1', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/enemigos/Enemigos-01.svg');
-        this.load.image('enemy2', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/enemigos/Enemigos-02.svg');
-        this.load.image('enemy3', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/enemigos/Enemigos-03.svg');
+        // ENEMIGOS - Rutas locales
+        this.load.image('enemy1', 'assets/images/enemigos/Enemigos-01.svg');
+        this.load.image('enemy2', 'assets/images/enemigos/Enemigos-02.svg');
+        this.load.image('enemy3', 'assets/images/enemigos/Enemigos-03.svg');
+        this.load.image('boss1', 'assets/images/enemigos/lobo.svg');
+        this.load.image('boss2', 'assets/images/enemigos/loboferoz.png');
+        this.load.image('boss3', 'assets/images/enemigos/lobotriste.png');
+        this.load.image('fire', 'assets/images/enemigos/fire.png');
 
-        // Cargar fondos
-        this.load.image('forest1', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/fondos/bosque1.jpg');
-        this.load.image('forest2', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/fondos/bosque2.jpg');
-        this.load.image('forest3', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/img/fondos/bosque3.jpg');
+        // FONDOS - Rutas locales
+        this.load.image('forest1', 'assets/images/fondos/bosque1.jpg');
+        this.load.image('forest2', 'assets/images/fondos/bosque2.jpg');
+        this.load.image('forest3', 'assets/images/fondos/bosque3.jpg');
+        this.load.image('forest4', 'assets/images/fondos/bosque4.jpg');
+        this.load.image('background_1_2', 'assets/images/fondos/background_1_2.jpeg');
+        this.load.image('background_final', 'assets/images/fondos/background_final.jpeg');
 
-        // Cargar audio
-        this.load.audio('background', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/sounds/background.mp3.mp3');
-        this.load.audio('jump', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/sounds/jump.mp3.mp3');
-        this.load.audio('hug', 'https://raw.githubusercontent.com/Evelez23/-oso.abrazos-/main/sounds/hug.mp3.mp3');
+        // AUDIO - Rutas locales
+        this.load.audio('background', 'assets/audio/background.mp3');
+        this.load.audio('jump', 'assets/audio/jump.mp3');
+        this.load.audio('collect', 'assets/audio/collect.mp3');
+        this.load.audio('hug', 'assets/audio/hug.mp3');
+        this.load.audio('hurt', 'assets/audio/hurt.mp3');
+        this.load.audio('enemy', 'assets/audio/enemy.mp3');
+        this.load.audio('powerup', 'assets/audio/powerup.mp3');
+        this.load.audio('shot', 'assets/audio/shot.mp3');
+        this.load.audio('bossBattle', 'assets/audio/boss_battle.mp3');
+        this.load.audio('intro', 'assets/audio/intro.mp3');
     }
 
     create() {
-        // Ir al menú principal después de cargar
         this.scene.start('MenuScene');
     }
 }
